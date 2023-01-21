ac636b31a91feca22e2237cc7c4e9515fb3f4fde
Trevor Stockdale <stockdale92@gmail.com>
2023-01-21 14:28:39 +0100

    added script.js

--- script.js
+++ script.js
@@ -8,16 +8,39 @@ const options = ["rock", "paper", "scissors"];
 function getComputerChoice() {
   const choice = options[Math.floor(Math.random() * options.length)];
   console.log(choice);
+  return choice;
 }
 
-getComputerChoice();
-
-function playGame(playerCh, compCh) {
-  if (playerCh === options) {
-    true;
+function checkWinner(playerCh, compCh) {
+  if (playerCh == compCh) {
+    return "ITS A DRAW!!";
+  } else if (
+    (playerCh == "rock" && compCh == "scissors") ||
+    (playerCh == "scissors" && compCh == "paper") ||
+    (playerCh == "paper" && compCh == "rock")
+  ) {
+    return "PLAYER WINS!!!";
   } else {
-    compCh === false;
+    return "Computer Wins!!";
   }
 }
 
+function playGame(playerCh, compCh) {
+  const result = checkWinner(playerCh, compCh);
+  if (result == "ITS A DRAW!!") {
+    return "It is a TIE NO ONE WINS";
+  } else if (result == "Player") {
+    return `You WON ${playerCh} beats ${compCh}`;
+  } else {
+    `You LOSE ${compCh} beats ${playerCh}`;
+    console.log(playerCh, compCh);
+  }
+}
+
+const playerChoice = "rock";
+const computerChoice = getComputerChoice();
+
 playGame();
+getComputerChoice();
+checkWinner();
+console.log(playGame(playerChoice, computerChoice));
